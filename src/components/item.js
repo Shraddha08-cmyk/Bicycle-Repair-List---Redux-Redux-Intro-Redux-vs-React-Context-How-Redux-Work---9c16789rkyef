ad079641af24902846bd71a0ff29909070c2dc0f
Shraddha Tiwari <114010000+Shraddha08-cmyk@users.noreply.github.com>
2023-01-06 19:38:03 +0530

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -1,82 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { repairAdded, repairUpdate } from "../actions";
+import React from "react";
+import { repairRemoved, repairResolved, editTask } from "../actions";
+import { useDispatch } from "react-redux";
 
-export default function Form() {
+export default function Item({ item }) {
+  const { id, owner, model, description, resolved } = item;
   const dispatch = useDispatch();
-  const item = useSelector((state) => state.bicycle.item);
-  const editMode = useSelector((state) => state.bicycle.editMode);
-  const [owner, setOwner] = useState(item.owner);
-  const [model, setModel] = useState(item.model);
-  const [id, setId] = useState(item.id);
-  const [description, setDescription] = useState(item.description);
-
-  useEffect(() => {
-    setOwner(item.owner);
-    setModel(item.model);
-    setDescription(item.description);
-    setId(item.id);
-  }, [item]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (editMode) {
-      dispatch(
-        repairUpdate({
-          id,
-          owner,
-          model,
-          description,
-        })
-      );
-    } else {
-      dispatch(
-        repairAdded({
-          owner,
-          model,
-          description,
-        })
-      );
-    }
-    setOwner("");
-    setModel("");
-    setDescription("");
+  const handleUpdate = () => {
+    dispatch(
+      editTask({
+        id: id,
+        owner: owner,
+        model: model,
+        description: description,
+        resolved: resolved,
+      })
+    );
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="owner-text-box">Owner</label>
-        <input
-          id="owner-text-box"
-          type="text"
-          name="owner"
-          value={owner}
-          onChange={(e) => setOwner(e.target.value)}
-          placeholder="owner"
-          required
-        />
-        <label htmlFor="model-text-box">Model</label>
-        <input
-          id="model-text-box"
-          type="text"
-          name="model"
-          value={model}
-          onChange={(e) => setModel(e.target.value)}
-          placeholder="model"
-          required
-        />
-        <label htmlFor="description-text-box">Description</label>
-        <input
-          id="description-text-box"
-          type="text"
-          name="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="description"
-          required
-        />
-        <button type="submit">+</button>
-      </form>
+    <div className="repair-item">
+      <span>{owner}</span>
+      <span>{model}</span>
+      <span>{description}</span>
+      {"-------- "}
+      <button onClick={handleUpdate}>Update</button>
+      <button onClick={(e) => dispatch(repairRemoved(id))}>Delete</button>
+      <button onClick={(e) => dispatch(repairResolved(id))}>
+        {!resolved ? "Done" : "Undo"}
+      </button>
     </div>
   );
 }
